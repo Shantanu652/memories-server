f0449aa3a9d2054d58c6f502e1d2cd62aefe1b31
Shantanu <sb652002@gmail.com>
2024-03-07 23:42:32 +0530

    Like Delete feature

--- controllers/posts.js
+++ controllers/posts.js
@@ -40,10 +40,21 @@ export const updatePost = async (req, res) => {
 export const deletePost = async(req,  res) => {
     const { id } = req.params;
 
-    if(!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send("No post is available with that id ")
+    if(!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send("No post is available with that id ")
 
     await PostMessage.findByIdAndUpdate(id);
-
+    console.log("DELETE");
     res.json({ message : 'Post deleted successfully' });
+}
+
+export const likePost = async(req, res) => {
+    const { id } = req.params;
+
+    if(!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send("No post is available with that id ")
+
+    const post = await PostMessage.findById(id);
+    const updatedPost = await PostMessage.findByIdAndUpdate(id, { likeCount: post.likeCount + 1 }, { new: true });
+
+    res.json(updatedPost);
 }
 
